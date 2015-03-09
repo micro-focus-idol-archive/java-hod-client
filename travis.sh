3fa94b1b39d20747d6cb1f8e043acad36aed2d4c
Alex Scown <alex.scown@hp.com>
2015-03-09 16:25:28 +0000

    Set up GitHub credentials on build server before generating the site.

--- travis.sh
+++ travis.sh
@@ -2,5 +2,7 @@
 
 if [[ $TRAVIS_BRANCH == 'master' ]]
 then
+  git config credential.helper "store --file=.git/credentials"
+  echo "https://${GITHUB_TOKEN}:@github.com" > .git/credentials
   mvn site -Prelease
 fi
