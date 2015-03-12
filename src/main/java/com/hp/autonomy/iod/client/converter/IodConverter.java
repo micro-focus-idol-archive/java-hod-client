0c3ec1c03bed188be09b51b0bc36b014ddf18f9d
Alex Scown <alex.scown@hp.com>
2015-03-12 18:37:06 +0000

    Document IodConverter.

--- src/main/java/com/hp/autonomy/iod/client/converter/IodConverter.java
+++ src/main/java/com/hp/autonomy/iod/client/converter/IodConverter.java
@@ -13,19 +13,34 @@ import retrofit.mime.TypedString;
 
 import java.lang.reflect.Type;
 
+/**
+ * {@link retrofit.converter.Converter} implementation that respects the {@link com.hp.autonomy.iod.client.converter.DoNotConvert}
+ * annotation.
+ */
 public class IodConverter implements Converter {
 
     private final Converter converter;
 
+    /**
+     * Creates a new IodConverter
+     * @param converter Delegate converter which will do most of the work
+     */
     public IodConverter(final Converter converter) {
         this.converter = converter;
     }
 
+    /**
+     * Returns the object returned by calling fromBody on the underlying converter
+     */
     @Override
     public Object fromBody(final TypedInput body, final Type type) throws ConversionException {
         return converter.fromBody(body, type);
     }
 
+    /**
+     * If object is annotated with {@link com.hp.autonomy.iod.client.converter.DoNotConvert}, returns a String
+     * representation of the object. Otherwise returns the object returned by calling toBody on the underlying converter
+     */
     @Override
     public TypedOutput toBody(final Object object) {
         if (object.getClass().isAnnotationPresent(DoNotConvert.class)) {
