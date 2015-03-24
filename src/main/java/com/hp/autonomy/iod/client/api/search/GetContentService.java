1cdb4e70d943189d231626a2a22a16eaf21c16e1
Alex Scown <alex.scown@hp.com>
2015-03-24 10:34:44 +0000

    Finish off the request interceptor methods

--- src/main/java/com/hp/autonomy/iod/client/api/search/GetContentService.java
+++ src/main/java/com/hp/autonomy/iod/client/api/search/GetContentService.java
@@ -16,7 +16,21 @@ public interface GetContentService {
     String URL = "/api/sync/getcontent/v1";
 
     /**
-     * Query IDOL OnDemand for documents matching query text
+     * Query IDOL OnDemand for documents matching query text using an API key provided by a {@link retrofit.RequestInterceptor}
+     * @param indexReference The reference list of the documents you want to view
+     * @param indexes The index the document resides in
+     * @param params Additional parameters to be sent as part of the request
+     * @return A list of documents that match the query text
+     */
+    @GET(URL)
+    Documents getContent(
+            @Query("index_reference") List<String> indexReference,
+            @Query("indexes") String indexes,
+            @QueryMap Map<String, Object> params
+    ) throws IodErrorException;
+
+    /**
+     * Query IDOL OnDemand for documents matching query text using the given API key
      * @param apiKey The API key to use to authenticate the request
      * @param indexReference The reference list of the documents you want to view
      * @param indexes The index the document resides in
