6464fe4e3f72674789baea0de255d0ff2497b3e4
Alex Scown <alex.scown@hp.com>
2015-03-24 12:36:57 +0000

    Add a version of TypedByteArray that provides a random filename, so IDOL OnDemand can accept files that are not really files. [rev: matthew.gordon]

--- src/test/java/com/hp/autonomy/iod/client/api/search/QueryTextIndexITCase.java
+++ src/test/java/com/hp/autonomy/iod/client/api/search/QueryTextIndexITCase.java
@@ -7,11 +7,16 @@ package com.hp.autonomy.iod.client.api.search;
 
 import com.hp.autonomy.iod.client.AbstractIodClientIntegrationTest;
 import com.hp.autonomy.iod.client.error.IodErrorException;
+import com.hp.autonomy.iod.client.util.TypedByteArrayWithFilename;
+import org.apache.commons.io.IOUtils;
 import org.junit.Before;
 import org.junit.Test;
 import retrofit.mime.TypedFile;
+import retrofit.mime.TypedOutput;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 import java.util.Map;
 
@@ -59,6 +64,24 @@ public class QueryTextIndexITCase extends AbstractIodClientIntegrationTest {
     @Test
     public void testQueryForFile() throws IodErrorException {
         final TypedFile file = new TypedFile("text/plain", new File("src/test/resources/com/hp/autonomy/iod/client/api/search/queryText.txt"));
+        final Map<String, Object> params = new QueryTextIndexRequestBuilder()
+            .setMaxPageResults(10)
+            .setAbsoluteMaxResults(10)
+            .addIndexes("wiki_ita", "wiki_eng")
+            .setSort(QueryTextIndexRequestBuilder.Sort.date)
+            .build();
+
+        final Documents documents = queryTextIndexService.queryTextIndexWithFile(getApiKey(), file, params);
+        final List<Document> documentList = documents.getDocuments();
+
+        assertThat(documentList, hasSize(10));
+    }
+
+    @Test
+    public void testQueryForFileAsStream() throws IodErrorException, IOException {
+        final InputStream stream = getClass().getResourceAsStream("/com/hp/autonomy/iod/client/api/search/queryText.txt");
+
+        final TypedOutput file = new TypedByteArrayWithFilename("text/plain", IOUtils.toByteArray(stream));
         final Map<String, Object> params = new QueryTextIndexRequestBuilder()
             .setMaxPageResults(10)
             .setAbsoluteMaxResults(10)
