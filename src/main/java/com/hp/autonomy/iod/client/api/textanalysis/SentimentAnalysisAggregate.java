24343015f8a4233fe7f723f7be6c64050d8ffcc1
Alex Scown <alex.scown@hp.com>
2015-03-26 16:03:55 +0000

    Implement the sentiment analysis API. [rev: matthew.gordon]

---
+++ src/main/java/com/hp/autonomy/iod/client/api/textanalysis/SentimentAnalysisAggregate.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2015 Hewlett-Packard Development Company, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
+package com.hp.autonomy.iod.client.api.textanalysis;
+
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+/**
+ * Holds the aggregate sentiment
+ */
+@Data
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@JsonDeserialize(builder = SentimentAnalysisAggregate.Builder.class)
+public class SentimentAnalysisAggregate {
+
+    /**
+     * @return The aggregate sentiment of the text
+     */
+    private final Sentiment sentiment;
+
+    /**
+     * @return The aggregate sentiment score of the text
+     */
+    private final double score;
+
+    @JsonPOJOBuilder(withPrefix = "set")
+    @Setter
+    @Accessors(chain = true)
+    public static class Builder {
+
+        private Sentiment sentiment;
+        private double score;
+
+        public SentimentAnalysisAggregate build() {
+            return new SentimentAnalysisAggregate(sentiment, score);
+        }
+
+    }
+
+}
