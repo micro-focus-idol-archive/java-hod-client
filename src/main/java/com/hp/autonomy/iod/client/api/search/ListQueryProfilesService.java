16cabd8ff9c5325830955d7effb9ff857bf36edb
Brian Beckett <brian.beckett@hp.com>
2015-04-20 17:41:28 +0100

    List Query Profiles implementation

---
+++ src/main/java/com/hp/autonomy/iod/client/api/search/ListQueryProfilesService.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2015 Hewlett-Packard Development Company, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
+package com.hp.autonomy.iod.client.api.search;
+
+import com.hp.autonomy.iod.client.error.IodErrorException;
+import retrofit.http.GET;
+import retrofit.http.Query;
+
+import java.util.List;
+
+public interface ListQueryProfilesService {
+
+    String URL = "/api/sync/listqueryprofiles/v1";
+
+    @GET(URL)
+	List<String> listQueryProfiles(
+            @Query("apiKey") String apiKey
+    ) throws IodErrorException;
+}
