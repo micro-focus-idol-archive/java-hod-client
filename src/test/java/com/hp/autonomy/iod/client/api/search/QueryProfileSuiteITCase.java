499d3c980abb409cc3de6cef3e3e9a8aeb6cef05
Brian Beckett <brian.beckett@hp.com>
2015-04-22 17:59:40 +0100

    Create test suite for Query Profile tests so we can ensure that code works before using it in other tests

---
+++ src/test/java/com/hp/autonomy/iod/client/api/search/QueryProfileSuiteITCase.java
@@ -0,0 +1,15 @@
+/*
+ * Copyright 2015 Hewlett-Packard Development Company, L.P.
+ * Licensed under the MIT License (the "License"); you may not use this file except in compliance with the License.
+ */
+
+package com.hp.autonomy.iod.client.api.search;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        CreateDeleteQueryProfileServiceSuiteChild.class
+})
+public class QueryProfileSuiteITCase {}
