d9a8ed9e1ea6bedde3391ebf14733d096188de4f
Matthew Gordon <matthew.gordon2@hp.com>
2015-05-29 15:26:31 +0100

    Added javadoc to ResourcesService.

--- src/main/java/com/hp/autonomy/hod/client/api/resource/ResourcesService.java
+++ src/main/java/com/hp/autonomy/hod/client/api/resource/ResourcesService.java
@@ -7,13 +7,27 @@ import retrofit.http.QueryMap;
 
 import java.util.Map;
 
+/**
+ * Interface representing the list resources API.
+ */
 public interface ResourcesService {
 
     String URL = "/2/api/sync/resource/v1";
 
+    /**
+     * Query HP Haven OnDemand for the list of resources using a token provided by a {@link retrofit.RequestInterceptor}.
+     * @param parameters Request parameters (can be built using {@link ListResourcesRequestBuilder}
+     * @return Public and private resources
+     */
     @GET(URL)
     Resources list(@QueryMap Map<String, Object> parameters);
 
+    /**
+     * Query HP Haven OnDemand for the list of resources with the given token
+     * @param token The authentication token
+     * @param parameters Request parameters (can be built using {@link ListResourcesRequestBuilder}
+     * @return Public and private resources
+     */
     @GET(URL)
     Resources list(@Header("token") AuthenticationToken token, @QueryMap Map<String, Object> parameters);
 
