8e48c97db4592e3814d89c680c6b9023c57f608f
Matthew Gordon <matthew.gordon2@hp.com>
2015-07-02 12:18:34 +0100

    Added java doc to Request class. [rev: none]

--- src/main/java/com/hp/autonomy/hod/client/util/Request.java
+++ src/main/java/com/hp/autonomy/hod/client/util/Request.java
@@ -5,6 +5,9 @@ import lombok.Data;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * Represents a request to HP Haven OnDemand.
+ */
 @Data
 public class Request {
     private final Verb verb;
