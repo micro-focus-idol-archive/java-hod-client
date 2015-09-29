a0f92250724a4d9e7e902428b7421585d409f687
Alex Scown <alex.scown@hp.com>
2015-09-29 17:33:33 +0100

    Add documentation

--- src/main/java/com/hp/autonomy/hod/client/api/resource/ResourceType.java
+++ src/main/java/com/hp/autonomy/hod/client/api/resource/ResourceType.java
@@ -12,17 +12,36 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * Represents the type of a resource in HP Haven OnDemand.
+ */
 @Data
 public class ResourceType implements Serializable {
 
     private static final long serialVersionUID = 5840146896341397940L;
 
+    /**
+     * The resource type of a text index
+     */
     public static final ResourceType CONTENT = new ResourceType("content");
+
+    /**
+     * The resource type of a connector
+     */
     public static final ResourceType CONNECTOR = new ResourceType("connector");
+
+    /**
+     * The resource type of a query profile
+     */
     public static final ResourceType QUERY_PROFILE = new ResourceType("query_profile");
 
     private final String name;
 
+    /**
+     * Creates a new resource type with the given name. The static fields declared on this class should be used in
+     * preference to this where possible.
+     * @param name The name of the resource type
+     */
     public ResourceType(final String name) {
         this.name = name;
     }
@@ -32,6 +51,9 @@ public class ResourceType implements Serializable {
         return name;
     }
 
+    /**
+     * @return A set containing all the ResourceType instances declared in static fields
+     */
     public static Set<ResourceType> allOf() {
         return new HashSet<>(Arrays.asList(
             CONTENT,
@@ -39,6 +61,7 @@ public class ResourceType implements Serializable {
             QUERY_PROFILE
         ));
     }
+
 
     public static Set<ResourceType> complementOf(final Set<ResourceType> resourceTypes) {
         final Set<ResourceType> set = allOf();
@@ -48,6 +71,12 @@ public class ResourceType implements Serializable {
         return resourceTypes;
     }
 
+    /**
+     * Returns a set containing the given resource types
+     * @param first The first resource type to add
+     * @param rest The remaining resource types to add
+     * @return A set containing the given resource types
+     */
     public static Set<ResourceType> of(final ResourceType first, final ResourceType... rest) {
         final Set<ResourceType> set = new HashSet<>();
         set.add(first);
