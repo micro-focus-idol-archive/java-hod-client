eb39bd3243b52138ca6fe4284f742ed7ca2a90d8
Alex Scown <alex.scown@hp.com>
2015-09-30 11:45:41 +0100

    Fix failing test

--- src/test/java/com/hp/autonomy/hod/client/api/resource/ResourcesTest.java
+++ src/test/java/com/hp/autonomy/hod/client/api/resource/ResourcesTest.java
@@ -24,12 +24,12 @@ public class ResourcesTest {
     @Test
     public void testSerializeAndDeserialize() throws IOException, ClassNotFoundException {
         final List<Resource> privateResources = Arrays.asList(
-            new Resource("foo", "bar", ResourceType.CONTENT, ResourceFlavour.EXPLORER, "yesterday"),
-            new Resource("bar", "bar", ResourceType.CONTENT, ResourceFlavour.EXPLORER, "yesterday")
+            new Resource("foo", "bar", ResourceType.CONTENT, ResourceFlavour.EXPLORER, "Wed Sep 30 2015 12:00:00 GMT+0000 (UTC)"),
+            new Resource("bar", "bar", ResourceType.CONTENT, ResourceFlavour.EXPLORER, "Wed Sep 30 2015 12:00:00 GMT+0000 (UTC)")
         );
 
         final List<Resource> publicResources = Collections.singletonList(
-            new Resource(ResourceIdentifier.NEWS_ENG.getName(), "bar", ResourceType.CONTENT, ResourceFlavour.EXPLORER, "yesterday")
+            new Resource(ResourceIdentifier.NEWS_ENG.getName(), "bar", ResourceType.CONTENT, ResourceFlavour.EXPLORER, "Wed Sep 30 2015 12:00:00 GMT+0000 (UTC)")
         );
 
         final Resources resources = new Resources(privateResources, publicResources);
