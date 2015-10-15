de5f0181726bdb74cd96cc68fc168fac90e1878c
Matthew Gordon <matthew.gordon2@hp.com>
2015-10-15 12:43:15 +0100

    Updated the ruby client and provisioning scripts so they can be pointed at different HOD endpoints. Use the PREVIEW endpoint on travis. [rev: alex.scown]

--- provision/setup-test-environment.rb
+++ provision/setup-test-environment.rb
@@ -2,17 +2,17 @@
 
 require_relative 'ruby-client'
 
-def create_text_index(token, index_name, index_flavor, proxy_host, proxy_port)
-  json = post("/api/async/textindex/#{index_name}/v2", proxy_host, proxy_port, :params => {:flavor => index_flavor}, :token => token)
+def create_text_index(endpoint, token, index_name, index_flavor, proxy_host, proxy_port)
+  json = post(endpoint, "/api/async/textindex/#{index_name}/v2", proxy_host, proxy_port, :params => {:flavor => index_flavor}, :token => token)
 
   job_id = json['jobID']
 
   # wait for completion
-  get("/job/#{job_id}/result", proxy_host, proxy_port, :token => token)
+  get(endpoint, "/job/#{job_id}/result", proxy_host, proxy_port, :token => token)
 end
 
-def add_to_text_index(token, filename, index_name, domain, proxy_host, proxy_port)
-  json = post("/api/async/textindex/#{domain}:#{index_name}/document/v1", proxy_host, proxy_port, :params => {
+def add_to_text_index(endpoint, token, filename, index_name, domain, proxy_host, proxy_port)
+  json = post(endpoint, "/api/async/textindex/#{domain}:#{index_name}/document/v1", proxy_host, proxy_port, :params => {
           :json => IO.read(filename),
           :duplicate_mode => 'replace'
       }, :token => token)
@@ -20,29 +20,34 @@ def add_to_text_index(token, filename, index_name, domain, proxy_host, proxy_por
   job_id = json['jobID']
 
   # wait for completion
-  get("/job/#{job_id}/result", proxy_host, proxy_port, :token => token)
+  get(endpoint, "/job/#{job_id}/result", proxy_host, proxy_port, :token => token)
 end
 
 apikey = ARGV[0]
-application = ARGV[1]
-domain = ARGV[2]
-proxy_host = ARGV[3]
-proxy_port = ARGV[4]
+environment = ARGV[1]
+application = ARGV[2]
+domain = ARGV[3]
+proxy_host = ARGV[4]
+proxy_port = ARGV[5]
 
-unless apikey && application && domain
-  abort("Usage: #{__FILE__} APIKEY APPLICATION DOMAIN [PROXY_HOST] [PROXY_PORT]")
+endpoint = resolve_endpoint(environment)
+
+unless endpoint && apikey && application && domain
+  abort("Usage: #{__FILE__} ENVIRONMENT APIKEY APPLICATION DOMAIN [PROXY_HOST] [PROXY_PORT]")
 end
 
-token = get_token(apikey, application, domain, proxy_host, proxy_port)
+puts "Using #{endpoint}"
 
-private_index_names = list_indexes(proxy_host, proxy_port, token)
+token = get_token(endpoint, apikey, application, domain, proxy_host, proxy_port)
+
+private_index_names = list_indexes(endpoint, proxy_host, proxy_port, token)
 
 # set up required indexes
 required_indexes = get_required_indexes
 
 required_indexes.reject{|index| private_index_names.include?(index[:name])}.each do |index|
   puts "Creating index with name #{index[:name]}"
-  create_text_index(token, index[:name], index[:flavor], proxy_host, proxy_port)
+  create_text_index(endpoint, token, index[:name], index[:flavor], proxy_host, proxy_port)
 end
 
-add_to_text_index(token, File.expand_path('../documents.json', __FILE__), required_indexes[0][:name], domain, proxy_host, proxy_port)
+add_to_text_index(endpoint, token, File.expand_path('../documents.json', __FILE__), required_indexes[0][:name], domain, proxy_host, proxy_port)
