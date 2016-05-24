aa7373a682d24b614c5d6c829e9edb55f0e96e88
Ivo Miller <ivo.miller@hpe.com>
2016-05-24 10:51:26 +0100

    FIND-132 Add controller method for retrieving date field values, for use in widget :: update aci_types library and allow hod ParametricSort enum to convert from idol SortParam enum [rev. alex.scown]

--- src/main/java/com/hp/autonomy/hod/client/api/textindex/query/parametric/ParametricSort.java
+++ src/main/java/com/hp/autonomy/hod/client/api/textindex/query/parametric/ParametricSort.java
@@ -5,14 +5,53 @@
 
 package com.hp.autonomy.hod.client.api.textindex.query.parametric;
 
+import com.hp.autonomy.types.requests.idol.actions.tags.params.SortParam;
+
 /**
  * Values for the GetParametricValues API sort parameter
  */
+@SuppressWarnings("WeakerAccess")
 public enum ParametricSort {
     document_count,
     alphabetical,
     reverse_alphabetical,
     number_increasing,
     number_decreasing,
-    off
+    off;
+
+    @SuppressWarnings("unused")
+    public static ParametricSort fromParam(final SortParam param) {
+        ParametricSort sort = off;
+        switch (param) {
+            case Alphabetical:
+                sort = alphabetical;
+                break;
+            case Date:
+                sort = number_decreasing; // HOD-2717
+                break;
+            case DocumentCount:
+                sort = document_count;
+                break;
+            case NumberDecreasing:
+                sort = number_decreasing;
+                break;
+            case NumberIncreasing:
+                sort = number_increasing;
+                break;
+            case ReverseAlphabetical:
+                sort = reverse_alphabetical;
+                break;
+            case ReverseDate:
+                sort = number_increasing; // HOD-2717
+                break;
+            case ReverseDocumentCount:
+                // HOD-2717
+                throw new IllegalArgumentException("Reverse document count sorting not currently supported in Haven On Demand");
+            case Off:
+                sort = off;
+                break;
+        }
+
+        return sort;
+    }
 }
