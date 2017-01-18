3e7f3fa9c3925beb9206f903e51827abc3abe509
Matthew Gordon <matthew.gordon2@hpe.com>
2017-01-18 12:31:14 +0000

    [FLIB-55] Added missing resource types.
    [rev: alex.scown]

--- src/main/java/com/hp/autonomy/hod/client/api/resource/ResourceType.java
+++ src/main/java/com/hp/autonomy/hod/client/api/resource/ResourceType.java
@@ -34,11 +34,27 @@ public class ResourceType implements Serializable {
      */
     public static final ResourceType QUERY_PROFILE = new ResourceType("query_profile");
 
+    /**
+     * The resource type of a language model
+     */
+    public static final ResourceType LANGUAGE_MODEL = new ResourceType("language_model");
+
+    /**
+     * The resource type of a user store
+     */
+    public static final ResourceType USER_STORE = new ResourceType("user_store");
+
+    /**
+     * The resource type of combinations
+     */
+    public static final ResourceType COMBINATIONS = new ResourceType("combinations");
+
     private final String name;
 
     /**
      * Creates a new resource type with the given name. The static fields declared on this class should be used in
      * preference to this where possible.
+     *
      * @param name The name of the resource type
      */
     public ResourceType(final String name) {
@@ -55,36 +71,38 @@ public class ResourceType implements Serializable {
      */
     public static Set<ResourceType> allOf() {
         return new HashSet<>(Arrays.asList(
-            TEXT_INDEX,
-            CONNECTOR,
-            QUERY_PROFILE
+                TEXT_INDEX,
+                CONNECTOR,
+                QUERY_PROFILE,
+                LANGUAGE_MODEL,
+                USER_STORE,
+                COMBINATIONS
         ));
     }
 
     /**
      * Returns a set containing all the known resource types, except those is the given set
+     *
      * @param resourceTypes The resource types which will not be included in the returned set
      * @return The complement of the given set of resource types
      */
     public static Set<ResourceType> complementOf(final Set<ResourceType> resourceTypes) {
         final Set<ResourceType> set = allOf();
-
         set.removeAll(resourceTypes);
-
         return set;
     }
 
     /**
      * Returns a set containing the given resource types
+     *
      * @param first The first resource type to add
-     * @param rest The remaining resource types to add
+     * @param rest  The remaining resource types to add
      * @return A set containing the given resource types
      */
     public static Set<ResourceType> of(final ResourceType first, final ResourceType... rest) {
         final Set<ResourceType> set = new HashSet<>();
         set.add(first);
         set.addAll(Arrays.asList(rest));
-
         return set;
     }
 
